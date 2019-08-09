bb773732ce511af348ab7afdcaeaba570c8927e6
Yonatan Gross <yonatan2gross@gmail.com>
2019-08-09 11:05:15 +0300

    added maven support. closed #3.

--- src/server_side/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author yonatan
- */
-/**
- * @author yonatan
- *
- */
-package server_side;
--- src/server_side/server/Server.java
+++
@@ -1,9 +0,0 @@
-package server_side.server;
-
-import server_side.clientHandling.ClientHandler;
-
-public interface Server {
-    public void open(int port, ClientHandler clientHandler);
-
-    public void stop();
-}
